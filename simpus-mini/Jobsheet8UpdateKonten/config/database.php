fc985bcb14b50097a65b884eb45a386ff3a5d46c
finnsz <xpvppr0@gmail.com>
2026-09-21 13:29:02 +0700

    update

--- simpus-mini/Jobsheet8UpdateKonten/config/database.php
+++ simpus-mini/Jobsheet8UpdateKonten/config/database.php
@@ -6,7 +6,8 @@ $user     = getenv('user') ?: "postgres";
 $password = getenv('password') ?: "123";
 
 try {
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;";
+    // sslmode=require wajib untuk jaringan Supabase Transaction Pooler
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
     $pdo = new PDO($dsn, $user, $password, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
